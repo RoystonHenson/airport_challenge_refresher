9ed27befc19c8f4018b6c54e10d0781d45868670
Royston Henson <rlqhenson@hotmail.com>
2018-03-06 17:48:27 +0000

    Add feature: random stormy/sunny state to weather

--- lib/weather.rb
+++ lib/weather.rb
@@ -4,4 +4,14 @@ class Weather
   def initialize
     @stormy = false
   end
+
+  def stormy?
+    random_stormy_generator
+  end
+
+  private
+
+  def random_stormy_generator
+    rand(10) > 5 ? @stormy = true : @stormy = false
+  end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -6,4 +6,16 @@ describe Weather do
      expect(subject.stormy).to eq(false)
    end
   end
+
+  describe '#stormy?' do
+    it 'is stormy' do
+      allow(subject).to receive(:stormy?).and_return(true)
+      expect(subject).to be_stormy
+    end
+
+    it 'is sunny' do
+      allow(subject).to receive(:stormy?).and_return(false)
+      expect(subject).not_to be_stormy
+    end
+  end
 end
