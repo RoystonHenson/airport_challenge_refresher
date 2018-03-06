c45e41d33f50df2a65a1c629113c7018c98200ed
Royston Henson <rlqhenson@hotmail.com>
2018-03-06 15:57:59 +0000

    Add feature: plane can take off from airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,10 +1,16 @@
 require 'airport'
 
 describe Airport do
+  let(:plane) { double('plane') }
   describe '#land' do
     it 'lands a plane at the airport' do
-      plane = 'plane'
-      expect(subject.land(plane)).to eq('plane')
+      expect(subject.land(plane)).to eq(plane)
+    end
+  end
+
+  describe '#take_off' do
+    it 'plane takes off from the airport' do
+      expect(subject.take_off(plane)).to eq(plane)
     end
   end
 end
